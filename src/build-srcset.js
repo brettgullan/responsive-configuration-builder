1747d336a247f14db2740d0cfb8c12b57a0e9b00
Brett Gullan <brettgullan@mac.com>
2019-08-01 21:33:13 +1000

    wip: Working version of buildSrcSet function
    Needs some further refactoring, but the basic shape is there.

--- src/build-srcset.js
+++ src/build-srcset.js
@@ -1,13 +1,13 @@
 import {
+  always,
   compose,
-  concat,
-  converge,
+  cond,
   curry,
+  defaultTo,
+  isEmpty,
   join,
   map,
-  omit,
-  pick,
-  zipWith,
+  when,
 } from 'ramda'
 
 //-----------------------------------------------------------------------------
@@ -15,7 +15,7 @@ import {
 import {
   generateSizeForRatio,
   generateSizesForRatio,
-  buildSizesForRatio,
+  generateSizesForScale,
 } from './build-sizes'
 import replaceTokens from './replace-tokens'
 
@@ -30,43 +30,25 @@ export const buildSrc = curry((url, tokens, { options, ...spec }) => {
 
 //-----------------------------------------------------------------------------
 
-/*
-export const buildSrcSet = (url, { widths, ratio }, tokens) => {
-  let sizes = generateSizesForRatio(widths, ratio)
-  sizes = map((size) => replaceTokens(url, Object.assign(tokens, size)), sizes)
-  return sizes.join(', ')
-}
-
-
-export const buildSrcSet = (url, spec, tokens) => compose(
-  join(', '),
-  map((size) => replaceTokens(url, Object.assign(tokens, size))),
-  buildSizesForRatio
-)(spec)
-*/
-
-/*
-export const buildSrcSet = curry((url, tokens, { options, ...spec }) => {
-  const { widths, ratio } = spec
-  // TODO: work out which generate function to call -- size, scale, ratio, etc.
-  const sizes = generateSizesForRatio(widths, ratio, true)
-  return map(
-    (size) =>
-      replaceTokens(`${url} {width}w`, Object.assign(tokens, options, size)),
-    sizes,
-  ).join(', ')
-})
-*/
+const buildSizesForSrcSet = cond([
+  [
+    ({ widths, ratio }) => widths && ratio,
+    ({ widths, ratio }) => generateSizesForRatio(widths, ratio, true),
+  ],
+  [
+    ({ size, scale }) => size && scale,
+    ({ size, scale }) => generateSizesForScale(size, scale, true),
+  ],
+])
 
 export const buildSrcSet = curry((url, tokens, { options, ...spec }) => {
-  const { widths, ratio } = spec
-  // TODO: work out which generate function to call -- size, scale, ratio, etc.
-  const sizes = generateSizesForRatio(widths, ratio, true)
   return compose(
+    when(isEmpty, always(undefined)),
     join(', '),
-    converge(zipWith(concat), [
-      map((size) => replaceTokens(url, Object.assign(tokens, options, size))),
-      map((size) => replaceTokens(` {width}w`, size)),
-    ]),
-  )(sizes)
+    map((size) =>
+      replaceTokens(`${url} {width}w`, Object.assign(tokens, options, size)),
+    ),
+    defaultTo([]),
+    buildSizesForSrcSet,
+  )(spec)
 })
--- test/build-srcset.spec.js
+++ test/build-srcset.spec.js
@@ -30,13 +30,14 @@ describe.only('Build Src', () => {
 })
 
 describe.only('Build SrcSet', () => {
-  it('given width/height size object, returns same', () => {
-    const url = `https://picsum.photos/id/{id}/{width}/{height}?q={quality}&crop={crop}`
-    const tokens = {
-      id: 128,
-      quality: 80,
-      crop: 'auto',
-    }
+  const url = `https://picsum.photos/id/{id}/{width}/{height}?q={quality}&crop={crop}`
+  const tokens = {
+    id: 128,
+    quality: 80,
+    crop: 'auto',
+  }
+
+  it('return srcset string for ratio size spec', () => {
     const spec = {
       widths: ['240px', 320, 480],
       ratio: '16 / 9',
@@ -53,9 +54,40 @@ describe.only('Build SrcSet', () => {
 
     const result = buildSrcSet(url, tokens, spec)
 
-    console.log(result)
+    expect(result).to.equal(expected)
+  })
+
+  it('return srcset string for scale spec', () => {
+    const spec = {
+      size: { width: '240px', height: '180px' },
+      scale: [1, 1.5, 2, 3],
+      options: {
+        quality: 50,
+      },
+    }
+
+    const expected = [
+      'https://picsum.photos/id/128/240/180?q=50&crop=auto 240w',
+      'https://picsum.photos/id/128/360/270?q=50&crop=auto 360w',
+      'https://picsum.photos/id/128/480/360?q=50&crop=auto 480w',
+      'https://picsum.photos/id/128/720/540?q=50&crop=auto 720w',
+    ].join(', ')
+
+    const result = buildSrcSet(url, tokens, spec)
 
     expect(result).to.equal(expected)
+  })
+
+  it('return undefined for invalid spec', () => {
+    const spec = {
+      options: {
+        quality: 50,
+      },
+    }
+
+    const result = buildSrcSet(url, tokens, spec)
+
+    expect(result).to.equal(undefined)
   })
 })
 
