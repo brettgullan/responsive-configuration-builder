3a43d8a2b977c855d05e7334ebf87ea9490a1944
Brett Gullan <brettgullan@mac.com>
2019-08-01 11:16:01 +1000

    improve: Update evaluateRatio function to return ratio as expressed
    Previously we returned the inverse of the ratio, i.e. 16/9 resolved to 0.5625. Changed to return the actual value of the ratio, to be consistent with the values returned when this function is *not* used.

--- src/evaluate-ratio.js
+++ src/evaluate-ratio.js
@@ -6,45 +6,45 @@ const fractionalCaseRegex = /^\d+\/\d+$/
 /**
  * Convert common aspect-ratio expressions (width/height) into decimal values.
  * i.e:
- *    16/9 = 0.5625
- *    '16/9' = 0.5625
- *    '4:3' = 0.75
- *    '56.25%' = 0.5625
+ *    16/9 = 1.777*
+ *    '16/9' = 1.777*
+ *    '4:3' = 1.333*
+ *    '56.25%' = 1.777*
  * Useful when calculating height based on a width and ratio.
  *
  * @param {Number|String} ratio
  * @return {Number} ratio expressed as decimal width/height
  */
 export const evaluateRatio = (ratio) => {
+  if (!ratio) return undefined // sanity!
+
   let candidate = Number(ratio)
 
   // Anything that has dynamically resolved to a Number
   // 16/9, 1.7777778, etc.
-  if (candidate) {
-    return 1 / candidate
-  }
+  if (candidate) return candidate
 
   candidate = ratio.toString().trim()
 
   // percentage case
   if (candidate.slice(-1) === '%') {
-    return Number.parseFloat(candidate) / 100
+    return 1 / (Number.parseFloat(candidate) / 100)
   }
 
   // ratio case (width:height)
   if (candidate.search(ratioCaseRegex) > -1) {
     const [a, b] = candidate.split(':')
-    return b / a
+    return a / b
   }
 
   // fractional case (width/height)
   if (candidate.search(fractionalCaseRegex) > -1) {
     const [a, b] = candidate.split('/')
-    return b / a
+    return a / b
   }
 
   // wtf?
-  return 0
+  return undefined
 }
 
 export default evaluateRatio
--- test/evaluate-ratio.spec.js
+++ test/evaluate-ratio.spec.js
@@ -6,29 +6,34 @@ import evaluateRatio from '../src/evaluate-ratio'
 
 describe('evaluateRatio', () => {
   it('should handle percentages', () => {
-    expect(evaluateRatio('56.25%')).to.eql(0.5625)
-    expect(evaluateRatio('43.75%')).to.eql(0.4375)
+    expect(evaluateRatio('56.25%')).to.eql(16 / 9)
+    expect(evaluateRatio('75%')).to.eql(4 / 3)
   })
 
   it('should handle fractions', () => {
-    expect(evaluateRatio(16 / 9)).to.eql(0.5625)
+    expect(evaluateRatio(16 / 9)).to.eql(16 / 9)
   })
 
   it('will also invert decimals! This is correct behaviour.', () => {
     // 16/9 is 1.7777*, though to calculate height based on width resolves to 1/1.7777*
     // Similarly 9/16 is 0.5625; to calculate height based on width resolves to 16/9 (or 1.7777*)
-    expect(evaluateRatio(0.5625)).to.equal(16 / 9)
+    expect(evaluateRatio(0.5625)).to.equal(9 / 16)
   })
 
   it('should handle fraction as strings', () => {
-    expect(evaluateRatio('16/9')).to.eql(0.5625)
+    expect(evaluateRatio('16/9')).to.eql(16 / 9)
   })
 
   it('should handle ratios', () => {
-    expect(evaluateRatio('4:3')).to.equal(0.75)
+    expect(evaluateRatio('4:3')).to.equal(4 / 3)
   })
 
   it('non-numeric values resolve to 0', () => {
-    expect(evaluateRatio('some thing else')).to.equal(0)
+    expect(evaluateRatio('some thing else')).to.equal(undefined)
+  })
+
+  it('should handle an undefined', () => {
+    expect(evaluateRatio()).to.eql(undefined)
+    expect(evaluateRatio(undefined)).to.eql(undefined)
   })
 })
