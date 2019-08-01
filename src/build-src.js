fdab8e91a1425d385b864f8944b36681e6646743
Brett Gullan <brettgullan@mac.com>
2019-08-02 09:01:03 +1000

    improve: Add generic size handler to src builder function
    - Refactor function to handle multiple spec formats/strategies
    - Add size format handling

--- src/build-src.js
+++ src/build-src.js
@@ -1,17 +1,34 @@
-import { curry } from 'ramda'
+import { always, compose, cond, curry, isEmpty, T, when } from 'ramda'
 
 //-----------------------------------------------------------------------------
 
-import { generateSizeForRatio } from './build-sizes'
+import { generateSizeForRatio, generateSizeForSize } from './build-sizes'
 import replaceTokens from './replace-tokens'
 
 //-----------------------------------------------------------------------------
 
-export const buildSrc = curry((url, tokens, { options, ...spec }) => {
-  const { width, ratio } = spec
-  // TODO: work out which generate function to call -- size, scale, ratio, etc.
-  const size = generateSizeForRatio(width, ratio, true)
-  return replaceTokens(url, Object.assign(tokens, options, size))
-})
+export const buildSrc = curry((url, tokens, { options, ...spec }) =>
+  compose(
+    // Return undefined (as error code) if result is empty string
+    when(isEmpty, always(undefined)),
+
+    // Build src string
+    (size) => replaceTokens(url, Object.assign(tokens, options, size)),
+
+    // Generate size object using appropriate function.
+    cond([
+      [
+        ({ width, ratio }) => width && ratio,
+        ({ width, ratio }) => generateSizeForRatio(width, ratio, true),
+      ],
+      [
+        ({ width, height }) => width && height,
+        ({ width, height }) => generateSizeForSize(width, height, true),
+      ],
+
+      [T, always({})],
+    ]),
+  )(spec),
+)
 
 export default buildSrc
--- test/build-src.spec.js
+++ test/build-src.spec.js
@@ -4,13 +4,30 @@ import { buildSrc } from '../src/build-src'
 
 //-----------------------------------------------------------------------------
 
-describe('Build Src', () => {
-  it('given width/height size object, returns same', () => {
-    const url = `https://picsum.photos/id/{id}/{width}/{height}?q={quality}&crop={crop}`
-    const tokens = {
-      id: 128,
-      crop: 'auto',
+describe.only('Build Src', () => {
+  const url = `https://picsum.photos/id/{id}/{width}/{height}?q={quality}&crop={crop}`
+  const tokens = {
+    id: 128,
+    crop: 'auto',
+  }
+
+  const expected = 'https://picsum.photos/id/128/320/180?q=50&crop=auto'
+
+  it('given width/height spec object, returns correct src string', () => {
+    const spec = {
+      width: '320px',
+      height: '180px',
+      options: {
+        quality: 50,
+      },
     }
+
+    const result = buildSrc(url, tokens, spec)
+
+    expect(result).to.equal(expected)
+  })
+
+  it('given width/ratio size object, returns correct src string', () => {
     const spec = {
       width: '320px',
       ratio: '16 / 9',
@@ -18,8 +35,6 @@ describe('Build Src', () => {
         quality: 50,
       },
     }
-
-    const expected = 'https://picsum.photos/id/128/320/180?q=50&crop=auto'
 
     const result = buildSrc(url, tokens, spec)
 
