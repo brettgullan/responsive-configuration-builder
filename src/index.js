098fadb223d538e82ced4c39a237de797bb2fcab
Brett Gullan <brettgullan@mac.com>
2019-08-02 14:53:55 +1000

    feat: Export API functions

---
+++ src/index.js
@@ -0,0 +1,52 @@
+export { constructImage, constructPicture } from './construct-picture'
+
+export { buildSrcSet } from './build-srcset'
+export { buildSrc } from './build-src'
+
+export { evaluateRatio } from './evaluate-ratio'
+
+export {
+  generateSizeForSize,
+  generateSizeForScale,
+  generateSizeForRatio,
+  generateSizesForScale,
+  generateSizesForRatio,
+  buildSizesForRatio,
+  buildRatiosForRatio,
+} from './build-sizes'
+
+//-----------------------------------------------------------------------------
+
+import { constructImage, constructPicture } from './construct-picture'
+
+import { buildSrcSet } from './build-srcset'
+import { buildSrc } from './build-src'
+
+import { evaluateRatio } from './evaluate-ratio'
+
+import {
+  generateSizeForSize,
+  generateSizeForScale,
+  generateSizeForRatio,
+  generateSizesForScale,
+  generateSizesForRatio,
+  buildSizesForRatio,
+  buildRatiosForRatio,
+} from './build-sizes'
+
+//-----------------------------------------------------------------------------
+
+export default {
+  constructImage,
+  constructPicture,
+  generateSizeForSize,
+  generateSizeForScale,
+  generateSizeForRatio,
+  generateSizesForScale,
+  generateSizesForRatio,
+  buildSizesForRatio,
+  buildRatiosForRatio,
+  buildSrcSet,
+  buildSrc,
+  evaluateRatio,
+}
